e392b0906b97af80365cc0ccc03abafec6b4f8d7
vanhog <dieter@hoogestraat.dk>
2026-02-10 14:44:05 +0100

    First complete version with many, many mistakes

--- src/script.js
+++ src/script.js
@@ -1,77 +1,81 @@
 const category = {
-  Work: { icon: '/images/icon-work.svg', color: 'bg-orange-300a' },
-  Play: { icon: '/images/icon-play.svg', color: 'bg-navy-200' },
-  Study: { icon: '/images/icon-study.svg', color: 'bg-pink-300' },
-  Exercise: { icon: '/images/icon-exercise.svg', color: 'bg-green-400' },
-  Social: { icon: '/images/icon-social.svg', color: 'bg-purple-400' },
-  'Self Care': { icon: '/images/icon-self-care.svg', color: 'bg-yellow-300' },
+  Work: { icon: "/images/icon-work.svg", color: "bg-orange-300a" },
+  Play: { icon: "/images/icon-play.svg", color: "bg-navy-200" },
+  Study: { icon: "/images/icon-study.svg", color: "bg-pink-300" },
+  Exercise: { icon: "/images/icon-exercise.svg", color: "bg-green-400" },
+  Social: { icon: "/images/icon-social.svg", color: "bg-purple-400" },
+  "Self Care": { icon: "/images/icon-self-care.svg", color: "bg-yellow-300" },
 };
 
 cardDataHeaderClasses = [
-  'flex',
-  'flex-row',
-  'justify-between',
-  'items-center',
-  'w-full',
-  'font-rubik',
-  'font-medium',
-  'text-[1.25rem]',
-  'leading-5.25',
-  'tracking-normal',
-  'text-white',
+  "flex",
+  "flex-row",
+  "justify-between",
+  "items-center",
+  "w-full",
+  "font-rubik",
+  "font-regular",
+  "text-[1.25rem]",
+  "leading-5.25",
+  "tracking-normal",
+  "text-white",
 ];
 
 cardDataTimeClasses = [
-  'flex',
-  'flex-row',
-  'justify-between',
-  'items-center',
-  'w-full',
-  'text-white',
+  "flex",
+  "flex-row",
+  "justify-between",
+  "items-center",
+  "w-full",
+  "text-white",
+  "md:flex-row",
+  "md:flex-wrap",
 ];
 
-let trackingContainer = document.getElementById('tracking-container');
+let trackingContainer = document.getElementById("tracking-container");
 
 function makeCard(name, color, icon, hrsCurrent, hrsPrevious) {
-  let card = document.createElement('div');
+  let card = document.createElement("div");
   card.classList.add(`bg-[url('${category[name].icon}')]`); // ICON
   card.classList.add(category[name].color); // COLOR
-  card.classList.add('card-container');
+  card.classList.add("card-container");
 
-  let cardData = document.createElement('div');
-  cardData.classList.add('card-data');
-  cardData.setAttribute('tabindex', '0');
-  let cardDataHeader = document.createElement('div');
+  let cardData = document.createElement("div");
+  cardData.classList.add("card-data");
+  cardData.setAttribute("tabindex", "0");
+  let cardDataHeader = document.createElement("div");
 
   cardDataHeaderClasses.forEach((element) => {
     cardDataHeader.classList.add(element);
   });
 
-  let cardDataHeaderCategory = document.createElement('h2');
+  let cardDataHeaderCategory = document.createElement("h2");
   cardDataHeaderCategory.innerText = name; // NAME
-  let cardDataHeaderDots = document.createElement('button');
-  cardDataHeaderDots.setAttribute('aria-label', 'Open options');
-  cardDataHeaderDots.setAttribute('aria-haspopup', 'menu');
-  cardDataHeaderDots.setAttribute('aria-expanded', 'false');
+  let cardDataHeaderDots = document.createElement("button");
+  cardDataHeaderDots.setAttribute("aria-label", "Open options");
+  cardDataHeaderDots.setAttribute("aria-haspopup", "menu");
+  cardDataHeaderDots.setAttribute("aria-expanded", "false");
   cardDataHeaderDots.innerHTML =
     '<span class="hover:font-bold text-3xl" aria-hidden="true">...</span>';
   cardDataHeader.appendChild(cardDataHeaderCategory);
   cardDataHeader.appendChild(cardDataHeaderDots);
   cardData.appendChild(cardDataHeader);
 
-  let cardDataTime = document.createElement('div');
+  let cardDataTime = document.createElement("div");
   cardDataTimeClasses.forEach((element) => {
     cardDataTime.classList.add(element);
   });
 
-  let cardDataTimeHoursCurent = document.createElement('div');
-  cardDataTimeHoursCurent.classList.add('text-preset-3');
-  let cardDataTimeHoursPrevious = document.createElement('div');
-  cardDataTimeHoursPrevious.classList.add('text-navy-200');
-  cardDataTimeHoursCurent.innerHTML = `<span class="sr-only">Current - </span>${hrsCurrent}<span class="sr-only">hours</span><span aria-hidden="true">hrs</span>`;
+  let cardDataTimeHoursCurrent = document.createElement("div");
+  cardDataTimeHoursCurrent.classList.add("text-preset-3");
+  cardDataTimeHoursCurrent.classList.add("md:text-[56px]");
+  cardDataTimeHoursCurrent.classList.add("md:leading-[66px]");
+  let cardDataTimeHoursPrevious = document.createElement("div");
+  cardDataTimeHoursPrevious.classList.add("text-navy-200");
+  cardDataTimeHoursCurrent.innerHTML = `<span class="sr-only">Current - </span>${hrsCurrent}<span class="sr-only">hours</span><span aria-hidden="true">hrs</span>`;
 
   cardDataTimeHoursPrevious.innerHTML = `Previous - ${hrsPrevious}<span class="sr-only">hours</span><span aria-hidden="true">hrs</span>`;
-  cardDataTime.appendChild(cardDataTimeHoursCurent);
+  cardDataTime.appendChild(cardDataTimeHoursCurrent);
   cardDataTime.appendChild(cardDataTimeHoursPrevious);
   cardData.appendChild(cardDataTime);
 
@@ -80,15 +84,15 @@ function makeCard(name, color, icon, hrsCurrent, hrsPrevious) {
 }
 
 function populateTrackingContainer(inTimeframe) {
-  fetch('/data.json')
+  fetch("/data.json")
     .then((response) => {
-      if (!response.ok) return console.log('Oops! Something went wrong.');
+      if (!response.ok) return console.log("Oops! Something went wrong.");
 
       return response.json();
     })
     .then((data) => {
       if (trackingContainer.hasChildNodes()) {
-        trackingContainer.innerHTML = '';
+        trackingContainer.innerHTML = "";
       }
       data.forEach((element) => {
         trackingContainer.appendChild(
@@ -104,20 +108,20 @@ function populateTrackingContainer(inTimeframe) {
     });
 }
 
-const daily = document.getElementById('daily').addEventListener('click', () => {
-  populateTrackingContainer('daily');
+const daily = document.getElementById("daily").addEventListener("click", () => {
+  populateTrackingContainer("daily");
 });
 
 const weekly = document
-  .getElementById('weekly')
-  .addEventListener('click', () => {
-    populateTrackingContainer('weekly');
+  .getElementById("weekly")
+  .addEventListener("click", () => {
+    populateTrackingContainer("weekly");
   });
 
 const monthly = document
-  .getElementById('monthly')
-  .addEventListener('click', () => {
-    populateTrackingContainer('monthly');
+  .getElementById("monthly")
+  .addEventListener("click", () => {
+    populateTrackingContainer("monthly");
   });
 
-populateTrackingContainer('weekly');
+populateTrackingContainer("weekly");
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+// Source - https://stackoverflow.com/a/74179841
+// Posted by Mohammad Mustak Absar Khan
+// Retrieved 2026-02-10, License - CC BY-SA 4.0
+
+content: ["./*.{html,js}", "./src/**/*.{html,js}"];
