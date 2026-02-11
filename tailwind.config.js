677001d7e7506c83a65a9b4a0cc71a6e7ac21c8b
vanhog <dieter@hoogestraat.dk>
2026-02-11 12:32:41 +0100

    safelist classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,4 +2,16 @@
 // Posted by Mohammad Mustak Absar Khan
 // Retrieved 2026-02-10, License - CC BY-SA 4.0
 
-content: ["./*.{html,js}", "./src/**/*.{html,js}"];
+//content: ["./*.{html,js}", "./src/**/*.{html,js}"];
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts}"],
+  safelist: [
+    "bg-[url('../images/icon-work.svg')]",
+    "bg-[url('../images/icon-play.svg')]",
+    "bg-[url('../images/icon-study.svg')]",
+    "bg-[url('../images/icon-exercise.svg')]",
+    "bg-[url('../images/icon-social.svg')]",
+    "bg-[url('../images/icon-self-care.svg')]",
+  ],
+};
